5c752ba8436ab83839bba70d0c559565ee3824e1
sahil <sahilshityalkar05@gmail.com>
2023-01-02 22:18:49 +0530

    check the normal divs size

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello")
+let createDiv = document.getElementById('stetch-box')
+let btnblack = document.getElementById('blackButton')
+// let div = document.createElement("div")
+// div.style.width= "100px";
+// div.style.height= "100px";
+// div.style.background= "red";
+// createDiv.appendChild(div)
