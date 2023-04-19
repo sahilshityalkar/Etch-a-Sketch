1c7f85dded5483c9af0955afb6b9ef328dd3565f
sahil <sahilshityalkar05@gmail.com>
2023-04-19 14:19:17 +0530

    add new feature in createGrid funtion solve double creation of grid

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
+const containerGrid = document.getElementById("gridContainer")
+const innerContainer = document.getElementById("container")
+
 function createGrid(size) {
-    const container = document.getElementById("container");
-    container.style.display = "flex";
-    container.style.flexWrap = "wrap";
-    container.style.width = "600px";
-    container.style.height = "600px";
-    const squareSize = container.offsetWidth / size;
-  
-    for (let i = 0; i < size * size; i++) {
-      const square = document.createElement("div");
-      square.style.width = `${squareSize}px`;
-      square.style.height = `${squareSize}px`;
-      square.style.border = "1px solid black";
-      square.style.boxSizing = "border-box";
-      square.style.margin = "0"; // set margin to 0
-      square.style.padding = "0"; // set padding to 0
-      square.style.display = "inline-block"; // set display to inline-block
-      // square.addEventListener("mouseover", function() {
-      //   this.style.backgroundColor = "black";
-      // });
-      container.appendChild(square);
-    }
+  const container = document.getElementById('container');
+
+  // Remove existing grid if it exists
+  const existingGrid = container.querySelector('.grid');
+  if (existingGrid) {
+    container.removeChild(existingGrid);
   }
 
-  function reloadFunction(){
-    location.reload();
+  // Create new grid
+  const grid = document.createElement('div');
+  grid.className = 'grid';
+  grid.style.display = 'flex';
+  grid.style.flexWrap = 'wrap';
+  grid.style.width = '600px';
+  grid.style.height = '600px';
+
+  const squareSize = 600 / size;
+
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.className = 'allDivs';
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    square.style.border = '1px solid black';
+    square.style.boxSizing = 'border-box';
+    square.style.margin = '0';
+    square.style.padding = '0';
+    square.style.display = 'inline-block';
+    grid.appendChild(square);
   }
-  
-  function blackColorFunction(){
-    const allDivs = document.getElementById()
-  }
+  container.appendChild(grid);
+}
